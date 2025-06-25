d6fb4e7c6f50d49c61e8d5fe6f93dce8d0e1959a
Lovish Jaswal <lovishjaswal01@gmail.com>
2025-06-25 21:13:34 +0530

    Added basics of OOPs

---
+++ OOPs/Basic.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Dog{
+    public:
+    string name;
+    string breed;
+    void details(){
+        cout << "My dog's name is " << name << " and it's a " << breed << endl;
+    }
+};
+
+int main(){
+
+    Dog dog1;
+    dog1.name = "Bruno";
+    dog1.breed = "Pitbull";
+    dog1.details();
+
+    Dog dog2;
+    dog2.name = "Rocky";
+    dog2.breed = "Husky";
+    dog2.details();
+
+    Dog dog3;
+    dog3.name = "Charlie";
+    dog3.breed = "Labrador";
+    dog3.details();
+
+    return 0;
+}
